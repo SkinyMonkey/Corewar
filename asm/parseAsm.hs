766b6874d8c1add6f138e48fb689d1a11908509a
Adrien Candiotti <adrien.candiotti@gmail.com>
2015-04-16 00:39:33 +0200

    mod - found a solution to pass extracted data around

--- asm/parseAsm.hs
+++ asm/parseAsm.hs
@@ -25,34 +25,35 @@ parseOp candidate args
 parseInstruction' (token:args)
   | head token == '#' = True
   | head token == '.' = parseMetadata token $ intercalate "" args
-  | last token == ':' = parseLabel token && ((length(args) > 0 && (parseOp (head args) (tail args))) || True)
+  | last token == ':' = parseLabel token
+    && ((length(args) > 0 && (parseOp (head args) (tail args))) || True)
   | otherwise = parseOp token args
 
 parseInstruction tokens
   | (length tokens) > 0 = parseInstruction' tokens
   | (length tokens) == 0 = True
 
-worked index line True = putStr ""
-worked index line False = putStrLn $ "Syntax error \"" ++ line ++ "\" (line " ++ (show $ index + 1) ++ ")"
+worked index line True d = (True,d)
+worked index line False d =
+  error $ "Syntax error \"" ++ line ++ "\" (line " ++ (show $ index + 1) ++ ")"
 
--- FIXME : redefine with guards checking for empty list and use tail
-parseLine' lines index
-  | index < length(lines) = do
-      let line = lines !! index in
-        worked index line $ parseInstruction $ words line
-      parseLine' lines (index + 1)
-  | index == length(lines) = do
-      putStrLn "Syntax OK"
+-- Execution will stop if anything is False
+-- Now modify parseInstruction to take d to and use it
+parseLines' [line] index d = worked index line (parseInstruction $ words line) d
+parseLines' (lineHead:lineTail) index d =
+  (headRes && tailRes, tailD)
+  where 
+    (headRes, headD) = worked index lineHead (parseInstruction $ words lineHead) d
+    (tailRes, tailD) = parseLines' lineTail (index + 1) headD
 
-parseLine lines =
-  parseLine' lines 0
+parseLines lines = parseLines' lines 0 []
 
-finished True fileName = putStrLn $ "Compilation complete for " ++ fileName
-finished False fileName = putStrLn $ "Compilation failed for " ++ fileName
+finished fileName (True, d) = putStrLn $ "Compilation complete for " ++ fileName
+finished fileName (False, d) = putStrLn $ "Compilation failed for " ++ fileName
 
 generateChampion fileName = do
   championFile <- openFile fileName ReadMode
   content <- hGetContents championFile
-  parseLine $ lines content
+  finished fileName $ parseLines $ lines content
   generateCode $ lines content
   hClose championFile
