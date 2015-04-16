dffed462561617e7862f9d7225b3654773e25b5d
Adrien Candiotti <adrien.candiotti@gmail.com>
2015-04-16 02:29:11 +0200

    wtf to explain

--- asm/parseAsm.hs
+++ asm/parseAsm.hs
@@ -31,28 +31,28 @@ parseInstruction' (token:args)
   | otherwise = parseOp token args
 
 -- FIXME : here is next
-parseInstruction tokens
-  | (length tokens) > 0 = parseInstruction' tokens
-  | (length tokens) == 0 = True
+parseInstruction tokens cd
+  | (length tokens) > 0 = (parseInstruction' tokens, cd)
+  | (length tokens) == 0 = (True, cd)
+  where uCd = setCurrentLine cd tokens
 
-worked True cd = (True, cd)
-worked False cd =
+worked (True, cd) = (True, cd)
+worked (False, cd) =
   error $ "Syntax error \""
   ++ (getCurrentLine cd)
   ++ "\" (line "
   ++ (show $ (getLineNbr cd) + 1) ++ ")"
 
-parseLines' [line] cd = worked (parseInstruction $ words line)(setCurrentLine cd line)
+parseLines' [line] cd = worked (parseInstruction (words line) cd)
 parseLines' (lineHead:lineTail) cd =
   (headRes && tailRes, tailD)
-  where (headRes, headD) = worked (parseInstruction $ words lineHead) uCd
+  where (headRes, headD) = worked (parseInstruction (words lineHead) cd)
         (tailRes, tailD) = parseLines' lineTail (incLineNbr headD)
-        uCd = setCurrentLine cd lineHead
 
 parseLines lines cd = parseLines' lines $ setCurrentLine cd (head lines)
 
-finished fileName (True, d) = putStrLn $ "Compilation complete for " ++ fileName
-finished fileName (False, d) = putStrLn $ "Compilation failed for " ++ fileName
+finished fileName (True, cd) = putStrLn $ "Compilation complete for " ++ fileName
+finished fileName (False, cd) = putStrLn $ "Compilation failed for " ++ fileName
 
 generateChampion fileName = do
   championFile <- openFile fileName ReadMode
